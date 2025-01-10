f078d2d27488195d226aed11e970ecd1902bce03
KriteshTimsina <kriteshtimsina55@gmail.com>
2025-01-10 14:57:28 +0545

    feat: create project

--- app/index.tsx
+++ app/index.tsx
@@ -4,12 +4,15 @@ import { Colors } from '@/constants/Colors';
 
 import { Link, useNavigation } from 'expo-router';
 import { useSQLiteContext } from 'expo-sqlite';
-import { Pressable, ScrollView, Text, View } from 'react-native';
+import { Pressable, ScrollView, Text, ToastAndroid, View } from 'react-native';
 
 import { useEffect, useState } from 'react';
 import { useDrizzleStudio } from 'expo-drizzle-studio-plugin';
 import { useDb } from '@/db/useDb';
-import { projects } from '@/db/schema';
+import { projects as projectsSchema } from '@/db/schema';
+
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { count } from 'drizzle-orm';
 
 export default function Home() {
   const db = useDb();
@@ -17,12 +20,39 @@ export default function Home() {
 
   useEffect(() => {
     const load = async () => {
-      // const data = await db.insert(projects).values({ title: 'Test', description: 'text' });
       const data = await db.query.projects.findMany();
       setProjects(data);
     };
     load();
   }, []);
+
+  const onAddProject = async () => {
+    const hasProjects = await db.select({ count: count() }).from(projectsSchema);
+
+    if (hasProjects[0].count > 1) {
+      ToastAndroid.showWithGravity(
+        'Cannot add more than one project in free version',
+        ToastAndroid.LONG,
+        ToastAndroid.BOTTOM,
+      );
+      return false;
+    }
+
+    const data = await db
+      .insert(projectsSchema)
+      .values({
+        title: 'Project',
+        description: 'Hello',
+      })
+      .returning({
+        id: projectsSchema.id,
+        title: projectsSchema.title,
+      });
+
+    if (data) {
+      setProjects(data);
+    }
+  };
   return (
     <ThemedView
       style={{
@@ -34,11 +64,31 @@ export default function Home() {
 
       <ScrollView>
         <View style={{ gap: 10, marginVertical: 20 }}>
-          {projects.map((item, index) => {
-            return <Project key={index} item={item} />;
-          })}
+          {projects.length > 0 ? (
+            projects.map((item, index) => {
+              return <Project key={index} item={item} />;
+            })
+          ) : (
+            <ThemedText>No Projects</ThemedText>
+          )}
         </View>
       </ScrollView>
+      <Pressable
+        onPress={onAddProject}
+        style={{
+          backgroundColor: Colors.highlight,
+          height: 50,
+          width: 50,
+          borderRadius: 100,
+          justifyContent: 'center',
+          alignItems: 'center',
+          position: 'absolute',
+          bottom: '5%',
+          right: '5%',
+        }}
+      >
+        <AntDesign name="plus" size={35} color="white" />
+      </Pressable>
     </ThemedView>
   );
 }
