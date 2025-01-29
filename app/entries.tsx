0fd27ca1f7e6412508db2eb51c03ac313d70848a
KriteshTimsina <kriteshtimsina55@gmail.com>
2025-01-29 10:50:27 +0545

    refactor: extract input to a seperate component

--- app/entries.tsx
+++ app/entries.tsx
@@ -1,16 +1,18 @@
 import { Pressable, StyleSheet, View, TextInput, FlatList } from 'react-native';
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { router, useFocusEffect, useLocalSearchParams } from 'expo-router';
-import { Colors } from '@/constants/Colors';
+import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
+
 import { ThemedView } from '@/components/ThemedView';
 import { ThemedText } from '@/components/ThemedText';
-import { Ionicons } from '@expo/vector-icons';
-import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
-import Button from '@/components/Button';
 import EmptyProject from '@/components/EmptyProject';
-import { useEntries } from '@/store/entries';
+import InputText from '@/components/InputText';
 import Checklist from '@/components/Checklist';
+import Button from '@/components/Button';
+import { useEntries } from '@/store/entries';
 import { haptics } from '@/utils/haptics';
+
+import { Colors } from '@/constants/Colors';
 
 const Entries = () => {
   const bottomSheetRef = useRef<BottomSheet>(null);
@@ -102,35 +104,14 @@ const Entries = () => {
             <ThemedText type="subtitle" style={styles.sheetTitle}>
               Add New Task
             </ThemedText>
-            <View style={styles.inputContainer}>
-              <TextInput
-                ref={inputRef}
-                value={inputText}
-                onChangeText={setInputText}
-                placeholder="Enter your task title..."
-                placeholderTextColor="white"
-                style={[
-                  styles.input,
-                  {
-                    height: 40,
-                    textAlignVertical: 'top',
-                  },
-                ]}
-              />
-
-              <View style={styles.buttonContainer}>
-                <Pressable onPress={closeSheet} style={styles.iconButton}>
-                  <Ionicons name="close-outline" size={25} color="white" />
-                </Pressable>
-
-                <Pressable
-                  onPress={handleAddEntry}
-                  style={[styles.iconButton, { opacity: inputText.trim() ? 1 : 0.5 }]}
-                >
-                  <Ionicons name="paper-plane-outline" size={25} color="white" />
-                </Pressable>
-              </View>
-            </View>
+            <InputText
+              placeholder="Enter your task title..."
+              inputRef={inputRef}
+              inputText={inputText}
+              setInputText={setInputText}
+              onSubmit={handleAddEntry}
+              onClose={closeSheet}
+            />
           </View>
         </BottomSheetView>
       </BottomSheet>
@@ -153,45 +134,5 @@ const styles = StyleSheet.create({
   },
   sheetTitle: {
     textAlign: 'center',
-  },
-  inputContainer: {
-    backgroundColor: Colors.dark.shade,
-    borderRadius: 10,
-    padding: 10,
-  },
-  input: {
-    color: 'white',
-    fontSize: 16,
-    paddingHorizontal: 10,
-    paddingVertical: 8,
-    minHeight: 40,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    gap: 15,
-    marginTop: 10,
-  },
-  iconButton: {
-    padding: 5,
-  },
-  addButton: {
-    backgroundColor: Colors.highlight,
-    height: 50,
-    width: 50,
-    borderRadius: 100,
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    bottom: 30,
-    right: 20,
-    elevation: 5,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
   },
 });
---
+++ components/InputText.tsx
@@ -0,0 +1,111 @@
+import { Pressable, StyleSheet, TextInput, TextInputProps, View } from 'react-native';
+import React, { FC } from 'react';
+import { ThemedText } from './ThemedText';
+import { Ionicons } from '@expo/vector-icons';
+import { Colors } from '@/constants/Colors';
+import { MAX_INPUT_LENGTH } from '@/constants/constants';
+
+type InputTextProps = {
+  onSubmit: VoidFunction;
+  onClose: VoidFunction;
+  inputText: string;
+  setInputText: (text: string) => void;
+  inputRef: React.RefObject<TextInput>;
+} & TextInputProps;
+
+const InputText: FC<InputTextProps> = ({
+  inputRef,
+  inputText,
+  onClose,
+  onSubmit,
+  setInputText,
+  ...textInputProps
+}) => {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        ref={inputRef}
+        value={inputText}
+        onChangeText={setInputText}
+        placeholder="Enter your task title..."
+        placeholderTextColor="white"
+        style={styles.input}
+        {...textInputProps}
+      />
+
+      <View style={styles.counter}>
+        <ThemedText
+          style={[styles.countText, inputText.trim().length > MAX_INPUT_LENGTH && styles.error]}
+        >
+          {inputText.trim().length}/{MAX_INPUT_LENGTH}
+        </ThemedText>
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <Pressable hitSlop={5} onPress={onClose} style={styles.iconButton}>
+          <Ionicons name="close-outline" size={25} color="white" />
+        </Pressable>
+
+        <Pressable
+          onPress={onSubmit}
+          style={[styles.iconButton, { opacity: inputText.trim() ? 1 : 0.5 }]}
+        >
+          <Ionicons name="paper-plane-outline" size={25} color="white" />
+        </Pressable>
+      </View>
+    </View>
+  );
+};
+
+export default InputText;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    backgroundColor: Colors.dark.shade,
+    borderRadius: 10,
+    padding: 10,
+  },
+  input: {
+    color: 'white',
+    fontSize: 16,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    height: 40,
+    textAlignVertical: 'top',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    gap: 15,
+    marginTop: 10,
+  },
+  iconButton: {
+    padding: 5,
+  },
+  addButton: {
+    backgroundColor: Colors.highlight,
+    height: 50,
+    width: 50,
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 30,
+    right: 20,
+    elevation: 5,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+  counter: { position: 'absolute', right: 10, top: 0 },
+  countText: {
+    fontSize: 12,
+  },
+  error: {
+    color: 'red',
+  },
+});
