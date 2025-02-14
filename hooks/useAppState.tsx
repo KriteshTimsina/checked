f23076af99ebdbb29877d2d9da47bd56486a150f
KriteshTimsina <kriteshtimsina55@gmail.com>
2025-02-14 17:23:54 +0545

    feat: create a hook to check app state

---
+++ hooks/useAppState.tsx
@@ -0,0 +1,29 @@
+import { AppState } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+
+export type ApplicationState = AppState['currentState'];
+
+const useAppState = () => {
+  const appState = useRef(AppState.currentState);
+  const [appStateVisible, setAppStateVisible] = useState(appState.current);
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', nextAppState => {
+      if (appState.current.match(/inactive|background/) && nextAppState === 'active') {
+        console.log('App has come to the foreground!');
+      }
+
+      appState.current = nextAppState;
+      setAppStateVisible(appState.current);
+      console.log('AppState', appState.current);
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+  return appStateVisible;
+};
+
+export default useAppState;
