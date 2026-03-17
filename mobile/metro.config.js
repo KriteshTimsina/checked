f025aab825942f0d05b794c8c183a22209331dd0
kriteshtimsina <kriteshtimsina55@gmail.com>
2026-03-17 12:09:01 +1100

    fix: metro issue

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,7 +1,13 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 config.resolver.sourceExts.push('sql');
+
+config.watchFolders = [__dirname];
+
+// Restrict node_modules resolution to just mobile/node_modules
+config.resolver.nodeModulesPaths = [path.resolve(__dirname, 'node_modules')];
 module.exports = config;
