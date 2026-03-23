e9c22bc6cb2b78639e19a491b02a96828c5e731f
kriteshtimsina <kriteshtimsina55@gmail.com>
2026-03-23 20:30:13 +1100

    fix: drizzle file not found issue

---
+++ mobile/drizzle/0000_busy_rafael_vega.sql
@@ -0,0 +1,24 @@
+CREATE TABLE `entries` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`title` text NOT NULL,
+	`completed` integer DEFAULT false NOT NULL,
+	`created_at` integer NOT NULL,
+	`project_id` integer NOT NULL,
+	FOREIGN KEY (`project_id`) REFERENCES `projects`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE `notes` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`title` text NOT NULL,
+	`content` text,
+	`position` integer DEFAULT 0 NOT NULL,
+	`created_at` integer NOT NULL,
+	`updated_at` integer
+);
+--> statement-breakpoint
+CREATE TABLE `projects` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`title` text NOT NULL,
+	`description` text,
+	`created_at` integer NOT NULL
+);
