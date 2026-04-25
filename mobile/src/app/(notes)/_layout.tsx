afec0b64c8df57358431be654eb193f73d9a6bae
kriteshtimsina <kriteshtimsina55@gmail.com>
2026-04-25 12:12:12 +1000

    fix: platform specific back button

--- mobile/src/app/(notes)/_layout.tsx
+++ mobile/src/app/(notes)/_layout.tsx
@@ -3,9 +3,11 @@ import { router, Stack } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
 import { useTheme } from '@/hooks/useTheme';
 import { HapticButton } from '@/components/layout';
+import { Platform } from 'react-native';
 
 export default function NotesLayout() {
   const { icon } = useTheme();
+  const iconName = Platform.OS === 'android' ? 'arrow-back' : 'chevron-back';
   return (
     <Stack>
       <Stack.Screen
@@ -15,7 +17,7 @@ export default function NotesLayout() {
           headerTitle: '',
           headerLeft: () => (
             <HapticButton onPress={() => router.back()} hitSlop={10}>
-              <Ionicons name="chevron-back" size={24} color={icon} />
+              <Ionicons name={iconName} size={24} color={icon} />
             </HapticButton>
           ),
         }}
