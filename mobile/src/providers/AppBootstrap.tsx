c5b9e88c9ac56745946dd9c1a2a1173daba06157
kriteshtimsina <kriteshtimsina55@gmail.com>
2026-08-26 05:29:07 +1000

    chore: app bootstrap initialization logic

--- mobile/src/providers/AppBootstrap.tsx
+++ mobile/src/providers/AppBootstrap.tsx
@@ -1,22 +1,15 @@
 import { Splash } from '@/components/layout';
 import { useDatabaseInit } from '@/hooks/useDatabaseInit';
 import { useFontLoading } from '@/hooks/useFontLoading';
-import { usePreferences } from '@/hooks/usePreferences';
 import { SplashScreen } from 'expo-router';
 import { useEffect, useState } from 'react';
-import { Appearance } from 'react-native';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function AppBootstrap({ children }: { children: React.ReactNode }) {
   const { success: dbSuccess } = useDatabaseInit();
   const fontsLoaded = useFontLoading();
-  const { colorScheme } = usePreferences();
   const [showSplash, setShowSplash] = useState(true);
-
-  useEffect(() => {
-    Appearance.setColorScheme(colorScheme ?? 'light');
-  }, [colorScheme]);
 
   useEffect(() => {
     if (fontsLoaded && dbSuccess) {
